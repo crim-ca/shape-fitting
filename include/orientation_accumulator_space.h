778e73a932d10577b276ddb3706cf96ff5d9e651
Antoine Daigneault-Demers <antoine.daigneault-demers@crim.ca>
2022-04-11 14:55:17 -0400

    More adjustments

--- include/orientation_accumulator_space.h
+++ include/orientation_accumulator_space.h
@@ -14,7 +14,9 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #ifndef ORIENTATIONACCUMULATORSPACE_H
 #define ORIENTATIONACCUMULATORSPACE_H
 #include "boost/shared_ptr.hpp"
+#include "boost/thread/thread.hpp"
 #include "fitting_data.h"
+
 class OrientationAccumulatorSpace
 {
 	boost::shared_ptr<pcl::visualization::PCLVisualizer> sphereViz (pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud)
